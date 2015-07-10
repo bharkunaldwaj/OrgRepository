4f9f1d664b026f3bb0cf69a8e2a53a58ff346d65
AjayDamco <ajayk9@damcogroup.com>
2015-07-10 14:18:07 +0530

    On the participant set up screen when participants are entering their colleagues names and e-mail addresses, it often takes quite a while for the information to save, and the round icon that shows it is in the process of saving, is not always visible on the screen.  Can you see if there is anything that can be done to speed up the saving process or show that the system is in the process of saving, as people don’t realise that the data has not been saved and exit, losing all their information.

---
+++ feedback360/Layouts/Resources/js/AssignQuestionnaire.js
@@ -0,0 +1,42 @@
+﻿
+function ValidateRepeaterCandidateList() {
+
+    var reperaterCandidate = document.getElementById("RepeatorCandidateList");
+    var inputType = reperaterCandidate.getElementsByTagName("input");
+    var selectType = reperaterCandidate.getElementsByTagName("select");
+
+    var counter;
+    var invalidCounter = 0;
+    for (var i = 0; i < inputType.length; i++) {
+        var textBoxControlID = document.getElementById(inputType[i].id);
+
+        if (selectType[i] != undefined) {
+            var relationShipID = document.getElementById(selectType[i].id);
+
+            if (relationShipID.type == "select-one") {
+                var relationShipValue = relationShipID.value;
+
+                if (relationShipValue == "0") {
+                    relationShipID.style.borderColor = "red";
+                    invalidCounter = invalidCounter + 1;
+                }
+            }
+        }
+
+        if (textBoxControlID.type == "text") {
+            var resultValue = textBoxControlID.value;
+
+            if (resultValue == "") {
+                textBoxControlID.style.borderColor = "red";
+                invalidCounter = invalidCounter + 1;
+            }
+        }
+    }
+
+    if (invalidCounter > 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
