167adcb4a54747cc18e20f8adaa52dfabd7b941c
AjayDamco <ajayk9@damcogroup.com>
2015-07-27 19:37:04 +0530

    set up colleague save all as pr requirement

--- feedback360/Layouts/Resources/js/AssignQuestionnaire.js
+++ feedback360/Layouts/Resources/js/AssignQuestionnaire.js
@@ -1,37 +1,81 @@
 ﻿
 function ValidateRepeaterCandidateList() {
-
+    debugger;
     var reperaterCandidate = document.getElementById("RepeatorCandidateList");
     var inputType = reperaterCandidate.getElementsByTagName("input");
     var selectType = reperaterCandidate.getElementsByTagName("select");
 
     var counter;
     var invalidCounter = 0;
-    for (var i = 0; i < inputType.length; i++) {
+    for (var i = 0; i < selectType.length; i++) {
+
+        var ddlRelationship = (selectType[i].id).split("_ddlRelationship")[0];
+
+        var index = ddlRelationship.split("ctl00_cphMaster_rptrCandidateList_ctl")[1];
+
         var textBoxControlID = document.getElementById(inputType[i].id);
 
-        if (selectType[i] != undefined) {
-            var relationShipID = document.getElementById(selectType[i].id);
+        // if (selectType[i] != undefined) {
+        var relationShipID = document.getElementById(selectType[i].id);
 
-            if (relationShipID.type == "select-one") {
-                var relationShipValue = relationShipID.value;
+        if (relationShipID.type == "select-one") {
+            var relationShipValue = relationShipID.value;
 
-                if (relationShipValue == "0") {
+            var TextBoxName = document.getElementById("ctl00_cphMaster_rptrCandidateList_ctl" + index + "_txtName");
+            var textBoxEmail = document.getElementById("ctl00_cphMaster_rptrCandidateList_ctl" + index + "_txtEmailID");
+
+            if (relationShipValue == "0") {
+
+                if (TextBoxName.value != "" && textBoxEmail.value != "") {
                     relationShipID.style.borderColor = "red";
                     invalidCounter = invalidCounter + 1;
                 }
+                else if (TextBoxName.value == "" && textBoxEmail.value != "") {
+                    relationShipID.style.borderColor = "red";
+                    TextBoxName.style.borderColor = "red";
+                    invalidCounter = invalidCounter + 1;
+                }
+                else if (TextBoxName.value != "" && textBoxEmail.value == "") {
+                    relationShipID.style.borderColor = "red";
+                    textBoxEmail.style.borderColor = "red";
+                }
+                else {
+                    relationShipID.style.borderColor = "";
+                    TextBoxName.style.borderColor = "";
+                    textBoxEmail.style.borderColor = "";
+
+                    if (invalidCounter > 1)
+                        invalidCounter = invalidCounter - 1;
+                }
             }
-        }
+            else {
 
-        if (textBoxControlID.type == "text") {
-            var resultValue = textBoxControlID.value;
+                if (TextBoxName.value == "" && textBoxEmail.value == "") {
+                    TextBoxName.style.borderColor = "red";
+                    textBoxEmail.style.borderColor = "red";
+                    invalidCounter = invalidCounter + 1;
+                }
+                else if (TextBoxName.value != "" && textBoxEmail.value == "") {
 
-            if (resultValue == "") {
-                textBoxControlID.style.borderColor = "red";
-                invalidCounter = invalidCounter + 1;
+                    textBoxEmail.style.borderColor = "red";
+                    invalidCounter = invalidCounter + 1;
+                }
+                else if (TextBoxName.value == "" && textBoxEmail.value != "") {
+                    TextBoxName.style.borderColor = "red";
+                    invalidCounter = invalidCounter + 1;
+                }
+                else {
+                    relationShipID.style.borderColor = "";
+                    TextBoxName.style.borderColor = "";
+                    textBoxEmail.style.borderColor = "";
+
+                 if (invalidCounter>1)
+                     invalidCounter = invalidCounter - 1;
+                }
             }
         }
     }
+
 
     if (invalidCounter > 0) {
         return false;
