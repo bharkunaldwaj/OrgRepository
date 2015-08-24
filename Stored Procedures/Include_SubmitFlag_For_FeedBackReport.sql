ad9b24c4723a730640225b79a9f7e49406953959
AjayDamco <ajayk9@damcogroup.com>
2015-08-24 12:28:24 +0530

    function for 360 submit flag

---
+++ Stored Procedures/Include_SubmitFlag_For_FeedBackReport.sql
@@ -0,0 +1,22 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION Include_SubmitFlag_For_FeedBackReport()
+
+RETURNS bit
+AS
+BEGIN
+	
+	-- Return the result of the function
+	RETURN 0
+
+END
+GO
+
